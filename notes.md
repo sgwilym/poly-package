f4ed83a3c1839b1d8c2793b1a9b67b0e67fc4813
Sam Gwilym <gwilym@me.com>
2021-04-16 10:50:27 +0000

    Add a build using URL imports

--- notes.md
+++ notes.md
@@ -1,1 +1,3 @@
+- [] Unbundled, with URL imports
+
 Exporting conditional exports: https://nodejs.org/api/packages.html#packages_conditional_exports
---
+++ src/index.browser.js
@@ -0,0 +1,5 @@
+import * as InternetTime from 'https://cdn.skypack.dev/dot-beat-time';
+
+export function Howdy() {
+  console.log(`The time is ${InternetTime.now()}`);
+}
